073202ca79ada516a6ca479b9ee6141e6fc659cb
16bravo <cbravo2@yahoo.fr>
2024-01-27 00:47:09 +0100

    splitting the web files

---
+++ js/index.js
@@ -0,0 +1,87 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const yearSelect = document.getElementById('yearSelect');
+    const currentYear = new Date().getFullYear();
+
+    // Add an option for the current year (Last)
+    const lastOption = document.createElement('option');
+    lastOption.value = 'last';
+    lastOption.textContent = 'Last';
+    yearSelect.appendChild(lastOption);
+
+    // Add options for years from 1872 to current year
+    for (let year = currentYear-1; year >= 1872; year--) {
+    const option = document.createElement('option');
+    option.value = year.toString();
+    option.textContent = year.toString();
+    yearSelect.appendChild(option);
+    }
+
+    // Default JSON file path
+    let jsonFilePath = 'data/json/years/LastRankings.json';
+
+    // Function to load the JSON file
+    async function loadJSON(filePath) {
+    const response = await fetch(filePath);
+    const jsonData = await response.json();
+    return jsonData;
+    }
+
+    // Reference to table body
+    const tableBody = document.getElementById('table-body');
+
+    // Initial JSON loading
+    loadJSON(jsonFilePath).then(jsonData => {
+    // Loops through JSON data and constructs array rows
+    jsonData.forEach(item => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${item.ranking}</td>
+        <td data-toggle="tooltip" title="${item.ranking_change >= 0 ? '+' : ''}${item.ranking_change}">
+            ${item.ranking_change !== 0 ? `<i class="${item.ranking_change > 0 ? 'text-success' : 'text-danger'} fa fa-chevron-${item.ranking_change > 0 ? 'up style="color=green"' : 'down style="color=red"'}"></i>` : '<i class="fa fa-chevron-right" aria-hidden="true" style="color=gray"></i>'}
+        </td>
+        <td><img src="img/flags/${item.flag}" alt="${item.team}" width="30"></td>
+        <td><a href="matches.html?team=${item.reference_team.replace(/&/g, "%26")}">${item.team}</a></td>
+        <td>${item.points}</td>
+        <td>${item.points_change}</td>
+        <td>${item.confederation}</td>
+        `;
+        tableBody.appendChild(row);
+    });
+
+    // Activate DataTables on the array with Select
+    const dataTable = $('#myTable').DataTable({
+        //select: true  // Activate Select functionality
+    });
+
+    // Add an event manager to change the year
+    yearSelect.addEventListener('change', function () {
+        const selectedYear = this.value;
+        if (selectedYear === 'last') {
+        jsonFilePath = 'data/json/years/LastRankings.json';
+        } else {
+        jsonFilePath = `data/json/years/${selectedYear}Rankings.json`;
+        };
+        console.log(selectedYear);
+
+        // Empty existing table
+        dataTable.clear().draw();
+
+        // Load the new JSON and update the table
+        loadJSON(jsonFilePath).then(newJsonData => {
+        newJsonData.forEach(newItem => {
+            const newRow = dataTable.row.add([
+            newItem.ranking,
+            `<a data-toggle="tooltip" title="${newItem.ranking_change >= 0 ? '+' : ''}${newItem.ranking_change}">
+            ${newItem.ranking_change !== 0 ? `<i class="${newItem.ranking_change > 0 ? 'text-success' : 'text-danger'} fa fa-chevron-${newItem.ranking_change > 0 ? 'up style="color=green"' : 'down style="color=red"'}"></i>` : '<i class="fa fa-chevron-right" aria-hidden="true" style="color=gray"></i>'}
+            </a>`,
+            `<img src="img/flags/${newItem.flag}" alt="${newItem.team}" width="30">`,
+            `<a href="matches.html?team=${newItem.reference_team.replace(/&/g, "%26")}">${newItem.team}</a>`,
+            newItem.points,
+            newItem.points_change,
+            newItem.confederation
+            ]).draw(false).node();
+        });
+        });
+    });
+    });
+});
---
+++ js/matches.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Extract team name from the URL
+    const teamName = decodeURIComponent(new URLSearchParams(window.location.search).get('team'));
+
+    // Default JSON file path for team results (adjust as needed)
+    const jsonFilePath = `data/json/matches/${teamName}.json`;
+
+    // Modifiy page name and h1 title
+    document.title = `${teamName} Results`;
+    document.getElementById('h1title').textContent = teamName + " Results";
+
+    // Function to load team results JSON file
+    async function loadTeamResultsJSON(filePath) {
+    const response = await fetch(filePath);
+    const jsonData = await response.json();
+    return jsonData;
+    }
+
+    // Reference to team results table body
+    const teamResultsBody = document.getElementById('team-results-body');
+
+    // Initial JSON loading
+    loadTeamResultsJSON(jsonFilePath).then(teamResultsData => {
+    console.log('Team Results Data:', teamResultsData);
+
+    // Extracting flag value for title
+    const firstLine = teamResultsData.matches[0];
+    const flag1Value = firstLine.flag1;
+    const flagA = document.getElementById('flagTitle');
+    if (flagA) {
+        flagA.innerHTML = `<img src="img/flags/${flag1Value}" alt="${teamName}">`;
+    }
+
+    const matchesArray = teamResultsData.matches || [];
+    // Loop through team results data and construct table rows
+    matchesArray.forEach(match => {
+        var color;
+        if(match.score1 - match.score2 > 0) {
+        color = 'green';
+        }
+        else if (match.score1 - match.score2 < 0) {
+        color = 'red';
+        }
+        else {
+        color = 'black';
+        }
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${match.date}</td>
+        <td>${match.country}</td>
+        <td>${match.tournament}</td>
+        <td>vs <a href="matches.html?team=${match.team2.replace(/&/g, "%26")}"><img class="flag" src="img/flags/${match.flag2}" alt="${match.team2}"></a> <a href="matches.html?team=${match.team2}">${match.original_team2}</a></td>
+        <td>${Math.round(match.rating1-match.rating_ev)}</td>
+        <td>${Math.round(match.rating2+match.rating_ev)}</td>
+        <td>${match.win_prob} %</td>
+        <td class="match" style="color:${color};"><b>${match.score1} - ${match.score2}</b></td>
+        <td>${Math.round(match.rating1)} (${match.rating_ev >= 0 ? '+' : ''}${Math.round(match.rating_ev)})</td>
+        `;
+        teamResultsBody.appendChild(row);
+    });
+    });
+});
