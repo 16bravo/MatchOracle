0d9a1ec936d6f48acac53228570a21dcb431e6db
16bravo <cbravo2@yahoo.fr>
2024-01-27 19:21:05 +0100

    redefining teams init value + adding a gauge for Win%

--- js/matches.js
+++ js/matches.js
@@ -11,9 +11,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to load team results JSON file
     async function loadTeamResultsJSON(filePath) {
-    const response = await fetch(filePath);
-    const jsonData = await response.json();
-    return jsonData;
+        const response = await fetch(filePath);
+        const jsonData = await response.json();
+        return jsonData;
     }
 
     // Reference to team results table body
@@ -21,42 +21,61 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Initial JSON loading
     loadTeamResultsJSON(jsonFilePath).then(teamResultsData => {
-    console.log('Team Results Data:', teamResultsData);
+        console.log('Team Results Data:', teamResultsData);
 
-    // Extracting flag value for title
-    const firstLine = teamResultsData.matches[0];
-    const flag1Value = firstLine.flag1;
-    const flagA = document.getElementById('flagTitle');
-    if (flagA) {
-        flagA.innerHTML = `<img src="img/flags/${flag1Value}" alt="${teamName}">`;
-    }
+        // Extracting flag value for title
+        const firstLine = teamResultsData.matches[0];
+        const flag1Value = firstLine.flag1;
+        const flagA = document.getElementById('flagTitle');
+        if (flagA) {
+            flagA.innerHTML = `<img src="img/flags/${flag1Value}" alt="${teamName}">`;
+        }
 
-    const matchesArray = teamResultsData.matches || [];
-    // Loop through team results data and construct table rows
-    matchesArray.forEach(match => {
-        var color;
-        if(match.score1 - match.score2 > 0) {
-        color = 'green';
-        }
-        else if (match.score1 - match.score2 < 0) {
-        color = 'red';
-        }
-        else {
-        color = 'black';
-        }
-        const row = document.createElement('tr');
-        row.innerHTML = `
-        <td>${match.date}</td>
-        <td>${match.country}</td>
-        <td>${match.tournament}</td>
-        <td>vs <a href="matches.html?team=${match.team2.replace(/&/g, "%26")}"><img class="flag" src="img/flags/${match.flag2}" alt="${match.team2}"></a> <a href="matches.html?team=${match.team2}">${match.original_team2}</a></td>
-        <td>${Math.round(match.rating1-match.rating_ev)}</td>
-        <td>${Math.round(match.rating2+match.rating_ev)}</td>
-        <td>${match.win_prob} %</td>
-        <td class="match" style="color:${color};"><b>${match.score1} - ${match.score2}</b></td>
-        <td>${Math.round(match.rating1)} (${match.rating_ev >= 0 ? '+' : ''}${Math.round(match.rating_ev)})</td>
-        `;
-        teamResultsBody.appendChild(row);
-    });
+        const matchesArray = teamResultsData.matches || [];
+        // Loop through team results data and construct table rows
+        matchesArray.forEach(match => {
+            var color;
+            if(match.score1 - match.score2 > 0) {
+            color = 'green';
+            }
+            else if (match.score1 - match.score2 < 0) {
+            color = 'red';
+            }
+            else {
+            color = 'black';
+            }
+            const row = document.createElement('tr');
+            row.innerHTML = `
+            <td>${match.date}</td>
+            <td>${match.country}</td>
+            <td>${match.tournament}</td>
+            <td>vs <a href="matches.html?team=${match.team2.replace(/&/g, "%26")}"><img class="flag" src="img/flags/${match.flag2}" alt="${match.team2}"></a> <a href="matches.html?team=${match.team2}">${match.original_team2}</a></td>
+            <td>${Math.round(match.rating1-match.rating_ev)}</td>
+            <td>${Math.round(match.rating2+match.rating_ev)}</td>
+            <td>
+                <div class="percentage-indicator" data-percent="${match.win_prob}">
+                    <div class="fill"></div>
+                        <span class="text">${match.win_prob} %</span>
+                </div>
+            </td>
+            <td class="match" style="color:${color};"><b>${match.score1} - ${match.score2}</b></td>
+            <td>${Math.round(match.rating1)} (${match.rating_ev >= 0 ? '+' : ''}${Math.round(match.rating_ev)})</td>
+            `;
+            teamResultsBody.appendChild(row);
+        });
+
+        // % Win Gauge Indicator
+        const percentageIndicators = document.querySelectorAll('.percentage-indicator');
+
+        percentageIndicators.forEach(indicator => {
+            const percentValue = parseFloat(indicator.getAttribute('data-percent'));
+            const fillElement = document.createElement('div');
+            fillElement.classList.add('fill');
+            fillElement.classList.add('lv-'+Math.ceil(percentValue/20).toString());
+            fillElement.style.width = percentValue + '%';
+            indicator.innerHTML = '';
+            indicator.appendChild(fillElement);
+            indicator.innerHTML += `<span class="text">${percentValue}%</span>`;
+        });
     });
 });
