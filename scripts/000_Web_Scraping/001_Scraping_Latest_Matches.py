813b21d96453940751de745b0606615059331acf
16bravo <c.bravo2@yahoo.fr>
2024-02-08 15:58:40 +0100

    Update 001_Scraping_Latest_Matches.py

--- scripts/000_Web_Scraping/001_Scraping_Latest_Matches.py
+++ scripts/000_Web_Scraping/001_Scraping_Latest_Matches.py
@@ -17,7 +17,6 @@ chrome_options = Options()
 options = [
     "--headless",
     "--disable-gpu",
-    "--window-size=1920,1200",
     "--ignore-certificate-errors",
     "--disable-extensions",
     "--no-sandbox",
